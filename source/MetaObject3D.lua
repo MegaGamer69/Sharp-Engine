7e2c667af3c120474ae70f740a554c86a7a49c9a
MegaGamerOtario <megagamerotario@gmail.com>
2024-05-23 12:54:29 -0300

    updating

--- source/MetaObject3D.lua
+++ source/MetaObject3D.lua
@@ -4,10 +4,11 @@
 local MetaOBJ = {
     __index = {
         -- Cria uma Nova Instância de Meta-Objeto
-        Create = function(self, Name, Type)
-            local Instance = setmetatable({}, MetaOBJ)
-            Instance.Name = Name
-            Instance.Type = Type
+        Create = function(self, Name, Type, Parent)
+            local Instance  = setmetatable({}, MetaOBJ)
+            Instance.Name   = Name
+            Instance.Type   = Type
+            Instance.Parent = Parent
             
             -- Retorna a Instância Criada
             return Instance
@@ -16,6 +17,20 @@ local MetaOBJ = {
         -- Defina um Nome para o Meta-Objeto
         SetName = function(self, NewName)
             Instance.Name = NewName
+        end,
+        
+        Parent = function(self, NewParent)
+            Instance.Parent = NewParent
         end
     }
 }
+
+--[[ 
+-- Exemplo Genérico de Uso
+local Cube = MetaOBJ:Create("My Cube", "Spatial Model", "Main Scene")
+
+-- Imprima os Atributos
+print("Nome: " .. Cube.Name  )
+print("Tipo: " .. Cube.Type  )
+print("Pai : " .. Cube.Parent)
+]]
